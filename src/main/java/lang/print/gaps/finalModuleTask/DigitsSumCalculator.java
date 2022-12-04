011fdb63911bd33cea7ea74cfa5a97d13e782bc2
Andreyome <andreyomem@gmail.com>
2022-12-04 19:40:16 +0200

    Try 1

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int firstDigit = number %10;
+        number = number /10;
+        int secondDigit = number %10;
+        number = number /10;
+        int thirdDigit = number %10;
+        number = number /10;
+        int fourthDigit = number %10;
+        System.out.println(firstDigit+secondDigit+thirdDigit+fourthDigit);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String final = "";
+        final += number % 10;
+        number = number / 10;
+        final += number % 10;
+        number = number / 10;
+        final += number % 10;
+        System.out.println(final);
     }
 }
